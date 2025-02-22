ae02275df40710ae736f8ee976fed7346eb972ed
RebeccaMeegahapola <rebeccameegahapola@gmail.com>
2025-02-22 12:33:34 +0530

    chore: update product card

--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -1,18 +1,37 @@
 import Image from "next/image";
+import {useCartStore} from "@/store/cart-store";
 
 interface ProductProps {
+    id: number;
     image: string;
     name: string;
     price: number
 }
 
-export const ProductCard = ({ image, name, price}: ProductProps) => {
+export const ProductCard = ({ id, image, name, price}: ProductProps) => {
+    const { addToCart } = useCartStore();
     return (
         <div className="border p-4 rounded-lg shadow-md flex flex-col items-start">
             <Image src={image} alt="name" width={150} height={150} className="rounded-lg w-full"/>
             <h2 className="mt-3 text-md font-semibold">{name}</h2>
             <p className="text-[#d2b19a] text-sm mt-3">${price}</p>
-            <button className="mt-3 bg-[#1e120a] text-white text-sm px-4 py-2 rounded hover:bg-[#1e120a]">
+            <div className="mt-3 flex items-center gap-2 mb-3">
+                <button
+                    className="bg-gray-300 text-black px-2 py-1 rounded-xl"
+                >
+                    -
+                </button>
+                <span className="text-sm">quantity</span>
+                <button
+                    className="bg-gray-300 text-black px-2 py-1 rounded-xl"
+                >
+                    +
+                </button>
+            </div>
+            <button
+                className="mt-3 bg-[#1e120a] text-white text-sm px-4 py-2 rounded hover:bg-[#1e120a]"
+                onClick={() => addToCart({id, name, image, price, quantity: 1})}
+            >
                 Add to Cart
             </button>
         </div>
