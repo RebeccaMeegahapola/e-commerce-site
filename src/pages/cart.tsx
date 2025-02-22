3e8eb25a094618f9182474d179953670b6119739
RebeccaMeegahapola <rebeccameegahapola@gmail.com>
2025-02-22 15:50:10 +0530

    feat: add remove button to cart

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -3,28 +3,29 @@ import {Navbar} from "@/components/navbar";
 import {useCartStore} from "@/store/cart-store";
 
 const Cart = () => {
-    const { cart, updateQuantity } = useCartStore();
+    const { cart, updateQuantity, removeItem } = useCartStore();
+
     return (
         <>
             <Navbar/>
-            <div className='container mx-auto p-4'>
+            <div className='mx-auto p-8'>
                 <h1 className='text-2xl font-semibold mb-4'>Shopping Cart</h1>
                 {cart.length === 0 ? (
                     <p className='text-gray-500 text-md'>Your Cart is empty</p>
                 ) : (
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                    <div className="grid grid-cols-1 sm-grid-cols-2 md:grid-cols-5 gap-6">
                         {cart.map((product) => (
-                            <div key={product.id} className='border p-4 rounded-lg shadow-md flex flex-col items-center'>
-                                <Image src={product.image} alt={product.name} width={100} height={100} className="rounded-lg"/>
-                                <h2 className="mt-2 text-lg font-semibold">{product.name}</h2>
-                                <p className="text-gray-500">${product.price}</p>
+                            <div key={product.id} className='border p-4 rounded-lg shadow-md flex flex-col items-start'>
+                                <Image src={product.image} alt={product.name} width={150} height={150}
+                                       className="rounded-lg w-full"/>
+                                <h2 className="mt-2 text-sm font-semibold">{product.name}</h2>
+                                <p className="text-[#d2b19a] text-sm mt-3">${product.price}</p>
 
                                 {/* Quantity Controls */}
                                 <div className="mt-3 flex items-center gap-2">
                                     <button
                                         className="bg-gray-300 text-black px-2 py-1 rounded disabled:opacity-50"
                                         onClick={() => updateQuantity(product.id, -1)}
-                                        disabled={product.quantity <= 1}
                                     >
                                         -
                                     </button>
@@ -36,6 +37,12 @@ const Cart = () => {
                                         +
                                     </button>
                                 </div>
+                                <button
+                                    className="mt-3 bg-red-700 text-white text-sm px-4 py-2 rounded hover:bg-red-800"
+                                    onClick={() => removeItem(product.id)}
+                                >
+                                    Remove Item
+                                </button>
                             </div>
                         ))}
                     </div>
